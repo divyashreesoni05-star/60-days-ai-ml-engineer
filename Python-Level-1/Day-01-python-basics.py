ce33f834004f59def36233fda1afe09b83509c2c
Divyashree <divyashreesoni05@gmail.com>
2026-03-07 22:10:50 +0530

    Update Day-01-python-basics.py

--- Python-Level-1/Day-01-python-basics.py
+++ Python-Level-1/Day-01-python-basics.py
@@ -1,1 +1,32 @@
+# Day 01 - Python Basics
 
+# 1. Print Statement
+print("Hello World")
+
+# 2. Variables
+name = "Divya"
+age = 22
+
+print("My name is", name)
+print("My age is", age)
+
+# 3. Simple Calculation
+a = 5
+b = 3
+
+sum = a + b
+print("Sum:", sum)
+
+# 4. Checking Data Type
+x = 10
+y = "Python"
+
+print(type(x))
+print(type(y))
+
+# 5. Multiple Variables
+city = "Gwalior"
+branch = "IoT"
+
+print("City:", city)
+print("Branch:", branch)
